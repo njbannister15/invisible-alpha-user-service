695cc40fb20c51e9762c617d967981a16abfdc1b
nbannister <njbannister15@gmail.com>
2017-06-30 13:31:39 -0500

    new token

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ RUN npm install
 # Bundle app source
 COPY . /usr/src/app
 
-EXPOSE 8080
+EXPOSE 3000
 
 CMD [ "npm", "start" ]
 
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 var jwt = require('jsonwebtoken');
+var config  = require('../config');
 
 var opts = {
     secretOrKey: process.env.SECRET
@@ -11,6 +12,19 @@ var opts = {
 
 var User = require('../schema/user');
 var bcrypt = require('bcrypt');
+
+
+// Generate Unique Identifier for the access token
+function genJti() {
+  let jti = '';
+  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 16; i++) {
+      jti += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  
+  return jti;
+}
+
 
 router.post('/authenticate', (req, res, next) => {
     var query = User.where({ email: req.body.email });
@@ -25,17 +39,28 @@ router.post('/authenticate', (req, res, next) => {
             if (err) {
                 return res.status(500).send({ error: "Error in BCrypt compare" });
             } else if (result == true) {
-                var payload = {
+                
+                var token = jwt.sign({
                     id: user.id,
                     exp: Math.floor(Date.now() / 1000) + (60 * 60), // Signing a token with 1 hour of expiration
                     data: {
+                        email: user.email,
                         firstName: user.firstName,
-                        lastName: user.lastName,
-                        role: user.role
+                        lastName: user.lastName
                     }
-                };
-                var token = jwt.sign(payload, opts.secretOrKey);
-                return res.json({ message: "ok", token: token });
+                }, opts.secretOrKey);
+                
+                var accees = jwt.sign({
+                  iss: config.issuer,
+                  aud: config.audience,
+                  exp: Math.floor(Date.now() / 1000) + (60 * 60),
+                  scope: user.roles,
+                  sub: user.email,
+                  jti: genJti(), // unique identifier for the token
+                  alg: 'HS256'
+                }, opts.secretOrKey);
+
+                return res.json({ message: "ok", id_token: token, access_token: accees});
             } else {
                 return res.status(401).json({ error: "Bad Password" });
             }
